44e5406009d3a73693a7c7ba9c4a1b156c8a6914
Aayush Narnoli <112821990+Aayush2025@users.noreply.github.com>
2023-06-15 10:21:03 +0530

    Add files via upload
    comitted all codes of node and express js for making of calculator and bmi calculator.

---
+++ calculator.js
@@ -0,0 +1,36 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");     
+});
+app.post("/", function(req,res){
+   var num1 = Number(req.body.num1);
+   var num2 = Number(req.body.num2);
+   var result = num1 + num2;
+   
+    res.send("The result of variable is "+ result); 
+});
+
+app.get("/bmicalculator", function (req, res) {
+        res.sendFile(__dirname + "/bmicalculator.html");
+    });
+
+app.post("/bmicalculator", function(req, res){
+       var Weight = parseFloat(req.body.weigth);
+    var Height = parseFloat(req.body.height);
+
+    var Bmi = Weight/(Height * Height) ;
+      
+    res.send("Your BMI is "+ Bmi);
+});
+
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000.")
+});
